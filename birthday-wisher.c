9a928c12ce6a888343278ef644185c058a047c33
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-04-23 17:04:14 +0000

    feat: calculate current age, next age & days until next age

--- birthday-wisher.c
+++ birthday-wisher.c
@@ -1,29 +1,63 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <time.h>
 
-    int main() {
-        struct tm date1 = {0}, date2 = {0};
-        time_t time1, time2;
-        double seconds;
+int main() {
+    time_t current_time = time(NULL);
+    struct tm today = *localtime(&current_time);
 
-        // Set the first date
-        date1.tm_year = 2022 - 1900;  // Year is year since 1900
-        date1.tm_mon = 1;  // Month is 0 - 11
-        date1.tm_mday = 1;  // Day of the month is 1 - 31
+    struct tm input_date = {0};
 
-        // Set the second date
-        date2.tm_year = 2022 - 1900;
-        date2.tm_mon = 1;
-        date2.tm_mday = 31;
+    printf("Enter your birth year: ");
+    scanf("%d", &input_date.tm_year);
+    input_date.tm_year -= 1900;
 
-        // Convert to time_t
-        time1 = mktime(&date1);
-        time2 = mktime(&date2);
+    printf("Enter your birth month: ");
+    scanf("%d", &input_date.tm_mon);
+    input_date.tm_mon -= 1;
 
-        // Calculate the difference in seconds
-        seconds = difftime(time2, time1);
+    printf("Enter your birth day: ");
+    scanf("%d", &input_date.tm_mday);
 
-        printf("Difference between dates is %.2f seconds\n", seconds);
+    struct tm birthdate_in_current_year = {
+        .tm_year = today.tm_year,
+        .tm_mon = input_date.tm_mon,
+        .tm_mday = input_date.tm_mday,
+    };
 
-        return 0;
+    struct tm birthdate_in_next_year = {
+        .tm_year = today.tm_year + 1,
+        .tm_mon = input_date.tm_mon,
+        .tm_mday = input_date.tm_mday,
+    };
+
+    // Convert to time_t
+    time_t input_time = mktime(&input_date);
+    time_t current_birthtime = mktime(&birthdate_in_current_year);
+    time_t next_birthtime = mktime(&birthdate_in_next_year);
+
+    // Calculate the age
+    double seconds = difftime(current_time, input_time);
+    double age = seconds / (60 * 60 * 24 * 365);
+
+    double next_seconds, next_age;
+    // Check if today is the birthday
+    if (current_time < current_birthtime) {
+        next_seconds = difftime(current_birthtime, current_time);
+    } else {
+        next_seconds = difftime(next_birthtime, current_time);
     }
+    next_age = (next_seconds / (60 * 60 * 24 * 365)) + age;
+
+    // Calculate the remaining_days until the next birthday
+    double remaining_days = next_seconds / (60 * 60 * 24);
+
+    if (today.tm_mon == input_date.tm_mon &&
+        today.tm_mday == input_date.tm_mday) {
+        printf("Happy Birthday! \nYou are %.2f years old today.\n", age);
+    } else {
+        printf("You are %.2f years old.\nYou will be %.0f in %0.f days.\n\n",
+               age, next_age, remaining_days);
+    }
+
+    return 0;
+}
